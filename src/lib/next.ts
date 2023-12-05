35f9fac5ecba0401079df19cd40f7dc87173e527
Wyatt Johnson <accounts+github@wyattjoh.ca>
2023-12-05 09:33:21 -0700

    feat: enhance next command to use config for next project location

--- src/lib/next.ts
+++ src/lib/next.ts
@@ -1,19 +1,31 @@
-import { createCommand } from "./execa.js";
+import path from "node:path";
+import { getConfig } from "./config.js";
+import { Command, createCommand } from "./execa.js";
 
-const next = createCommand(
-  "node",
-  [
-    "--trace-deprecation",
-    "--enable-source-maps",
-    "packages/next/dist/bin/next",
-  ],
-  {
-    env: {
-      NEXT_TELEMETRY_DISABLED: "1",
-    },
+let next: Command;
+
+const command: Command = async (args, options = {}) => {
+  const nextProjectPath = await getConfig("next_project_path");
+
+  if (!next) {
+    next = createCommand(
+      "node",
+      [
+        "--trace-deprecation",
+        "--enable-source-maps",
+        path.join(nextProjectPath, "packages/next/dist/bin/next"),
+      ],
+      {
+        env: {
+          NEXT_TELEMETRY_DISABLED: "1",
+        },
+      }
+    );
   }
-);
 
-export default next;
+  return next(args, options);
+};
 
-export const debug = next;
+export default command;
+
+export const debug = command;
