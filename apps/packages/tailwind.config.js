69882c1a09a7c912dcb5de70ba82969b9a8b1ad4
Wyatt Johnson <accounts+github@wyattjoh.ca>
2025-05-07 13:39:23 -0600

    removed app

--- apps/packages/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./app/**/*.tsx"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/typography")],
-};
